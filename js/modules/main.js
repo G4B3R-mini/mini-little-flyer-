4c0104c957ee2f55ac07276e2aac0bfa6a6eae4d
G4b3r.x <g4b3r.x@gmail.com>
2025-10-10 17:17:52 -0300

    upload do threejs vresio:153

---
+++ js/modules/main.js
@@ -0,0 +1,5 @@
+import { Ceu } from "./ceu.js";
+import { IsolatedButton } from "./Isolated-button.js";
+import { Light } from "./Light.js";
+import { LoadGltf } from "./LoadGltf.js";
+import { Sol } from "./sol.js";
