71c1ec3b0b82f1108231327402b5bf211cce2cda
Gabriel marques do nascimento <gm2042225@gmail.com>
2025-11-19 07:23:33 -0300

    codigo base
    ## js\core\game-core.js:
    o coracao do jogo, onde junta todas as engines e poen para funcionar

--- js/core/game-core.js
+++ js/core/game-core.js
@@ -1,6 +1,32 @@
 class GameCore {
- constructor(){
-  
- }
- 
+  constructor(track, Move) {
+     this.move = new Move(0.1);
+    this.track = track;
+    this.scene = null;
+    this.camera = null;
+    this.renderer = null;
+    this.airplane = null;
+  }
+    __init() {
+    this.__addCanvas();
+    this.__createEnvironment();
+    this.__createAirplane();
+  }
+  __startAnimation() {
+    const animate = () => {
+      //this.time += 0.016;
+
+      // Animar avião (movimento suave de voo)
+      if (this.airplane) {
+        this.move.updateZ(this.airplane);
+      }
+
+      this.camera.lookAt(0, 0, 0);
+    };
+    super.__startAnimation(animate);
+  }
+  __createEnvironment() {
+    this.track.scene.position.z -= 10;
+    this.scene.add(this.track.scene);
+  }
 }
