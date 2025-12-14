4d153c33de105f10a109a72909a9005e8de02e14
g4b3r-linux <g4b3r.x@gmail.com>
2025-12-14 13:24:20 -0300

    feat: implementa Interface IObjectThreejs para contrato com objects Threejs (SRP, OCP)

---
+++ js-refactor/threejs/implemetation/IObjectThreejs.js
@@ -0,0 +1,14 @@
+export class IObjectThreejs{
+    get(){
+        throw new Error("method 'get' not implementad");
+        
+    }
+    addToScene(scene){
+          throw new Error("method 'addToScene' not implementad");
+    }
+    setPosition(x,y,z){
+          throw new Error("method 'setPosition' not implementad");
+    }
+}
+
+
