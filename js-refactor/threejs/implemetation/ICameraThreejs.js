32ceed502e6731dc643ebc9a11fcf418327aea9d
g4b3r-linux <g4b3r.x@gmail.com>
2025-12-14 13:22:09 -0300

    feat: implementa Interface ICameraThreejs para contrato de camerar (SRP, OCP)

---
+++ js-refactor/threejs/implemetation/ICameraThreejs.js
@@ -0,0 +1,12 @@
+export class ICameraThreejs{
+    get(){
+        throw new Error("method 'get' not implementad");
+        
+    }
+ setposition(x, y, z) {
+    throw new Error("method 'setposition' not implementad");
+  }
+   setLookAt(x, y, z) {
+    throw new Error("method 'setLookAt' not implementad");
+   }
+}
