f3d9897ec01095fc1fb7cfe12b06c3b3e8851d4a
g4b3r-linux <g4b3r.x@gmail.com>
2025-12-16 13:27:36 -0300

    feat: adiciona a Interface IGroup para contrato com gropos de modelos

---
+++ js-refactor/threejs/implemetation/IGroup.js
@@ -0,0 +1,15 @@
+
+export class IGroup {
+  add(object) {
+      throw new Error("Method 'add' not implemented");
+  }
+  get() {
+     throw new Error("Method 'get' not implemented");
+  }
+}
+
+export class IAppendObject {
+  add(obj_father, object) {
+ throw new Error("Method 'add' not implemented");
+  }
+}
