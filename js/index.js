18641dba59a47f91cad5c84471f785fbaa6a0368
g4b3r-linux <g4b3r.x@gmail.com>
2025-12-18 19:07:48 -0300

    fix: Corrigido o caminho de import e refatorado código para melhorar a leitura e funcionalidade. Além disso, adicionei uma impressão de logs detalhada no console para facilitar a verificação.

--- js/index.js
+++ js/index.js
@@ -2,30 +2,38 @@ import Setup from "./setup.js";
 import CacheLoader from "./cacheLoader.js";
 import { fakeEquipad } from "./models.js";
 import Phisic from "./core/physical-core.js";
+import { SetupCache } from "./Setup/SetupCache.js";
 
+let cls_cache = null;
 const libs = {
-  physic: 
-  await Phisic.create()
+  physic: await Phisic.create(),
 };
 
 const system = {
   conteiner: "screens",
-  libs: libs
+  libs: libs,
 };
 console.log("init");
-let cls_cache = null;
-const setup = new Setup(
-  system,
+
+const setupCache = new SetupCache(
+  2000,
   () => {
     cls_cache = new CacheLoader();
-    console.log("loading", cls_cache[fakeEquipad]);
+    console.log("loading", cls_cache.cache.length + 1);
     // lobby.addToScene(cls_cache[fakeEquipad].scene);
   },
   (lobby, game) => {
-    console.log(cls_cache.cache["cartoon"]);
+  
+for (const [key, value] of Object.entries(cls_cache.cache)) {
+  console.log(key, value);
+}
+
     const airplane = cls_cache.cache[fakeEquipad].scene;
     airplane.position.z = 3;
     lobby.setAirplane(airplane.clone());
     game.setAirplane(airplane.clone());
   }
 );
+
+
+const setup = new Setup(setupCache, system);
