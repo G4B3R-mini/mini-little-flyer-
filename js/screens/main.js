28d26092d9e190a21e4bde5f440a2728514469c7
g4b3r-linux <g4b3r.x@gmail.com>
2025-12-18 19:09:02 -0300

    fix: Remover duplicata e mudar o nome do arquivo para seguir padrões de nomenclatura uniforme.

--- js/screens/main.js
+++ js/screens/main.js
@@ -1,7 +1,7 @@
 import { Loading } from ".//loading.js";
 import { Lobby } from "./lobby.js";
 import { Game } from "./game.js";
-import { ScreenManager } from "./ScreenManager.js";
+import { ScreenManager } from "./screenManager.js";
 
 import { Screen } from "./screen-02.js";
 
