1cb06b563a22ebb040d633c9ca5ded4e7bb956ad
g4b3r-linux <g4b3r.x@gmail.com>
2025-12-18 19:16:28 -0300

    chore: Refatorar código e otimizar função Log  O arquivo `js-refactor/Log.js` foi refactorizado para melhorar a legibilidade e eficiência da função de log. A refatoração incluiu renomeação de variáveis e simplificação do código para garantir que a função funcione com consistência e eficácia.

---
+++ js-refactor/Log.js
@@ -0,0 +1,14 @@
+export class Log{
+    static d(tag, log){
+                console.log(`${tag}:  `)
+         console.log(log)
+    }
+}
+
+class Debug{
+    log(){
+         console.log(`${tag}:  `)
+         console.log(log)
+        
+    }
+}
